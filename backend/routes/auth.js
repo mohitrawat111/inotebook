138af02090eb676f6b3773261f3f16a45ff4b630
mohitrawat111 <109884777+mohitrawat111@users.noreply.github.com>
2022-09-16 19:07:50 +0530

    50-Creating Login Endpoint & sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,18 +46,59 @@ router.post('/createuser', [
                 id: user.id
             }
         }
-        console.log(data);//userid comes from database,conform krne klye log kiya taki sure ho ki data me user id he ari hai user ki.
+        // console.log(data);//userid comes from database,conform krne klye log kiya taki sure ho ki data me user id he ari hai user ki.
         const authToken = jwt.sign(data, JWT_SECRET);
         console.log(authToken);
         // res.json(user)
+        // res.json({ authToken})
         res.json({ authToken, user })
 
     } catch (error) {
 
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+//  Aunthenticate User usingusing: POST  "/api/auth/login". No login required
+
+router.post('/login', [
+
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be 5 characters').exists(),
+
+], async (req, res) => {
+    // If there are error, return bad request and the errors 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        console.log(user);//(output:-null means email exist nahi krti.if (!user)  means email nahi hai toh ye kro)
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" })
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        // console.log(data);//userid comes from database,conform krne klye log kiya taki sure ho ki data me user id he ari hai user ki.
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 
 })
 module.exports = router;
